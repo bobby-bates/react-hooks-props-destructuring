f1de622e6643af434f01ffab99fae48e53bca043
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-31 21:50:42 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,19 @@ function App() {
   const posterURL =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  const socialLinks = {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard 
+        title={title}
+        posterURL={posterURL}
+        genres={genresArr}
+        socialLinks={socialLinks}
+      />
     </div>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,14 +1,19 @@
 import React from "react";
-
+// Destructuring a nested object, socialLinks:
 function MovieCard({
-  title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
-  genres,
-}) {
+    title,
+    genres,
+    socialLinks: { github, linkedin },
+    posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg"
+})
+ 
+{
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
+      <h2>{github}</h2>
+      <h2>{linkedin}</h2>
       <small>{genres.join(", ")}</small>
     </div>
   );
